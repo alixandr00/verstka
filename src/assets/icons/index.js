4ea6312f97c6053d4e56c46b428766095086ab27
Ali <alisamatov106@gmail.com>
2023-12-20 21:10:10 +0600

    Updates

--- src/assets/icons/index.js
+++
@@ -1,5 +0,0 @@
-import LocationIcon from "./image 2.svg";
-import TelegramIcon from "./telegram.svg";
-import WhatsAppIcon from "./whatsapp.svg";
-import GroupsIcon from "./Group.svg";
-export { LocationIcon, TelegramIcon, WhatsAppIcon, GroupsIcon };
